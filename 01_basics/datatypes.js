b78b78038ea162889073c32d3ea43b03660e61e1
Jitendra Kumar Sahoo <160145574+jitendraK4121@users.noreply.github.com>
2025-02-04 01:41:30 +0000

    Datatype n variablese

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,12 @@
+let name = "jitu"
+let age = 22
+let isMarried =  false
+let hasJob = null
+let isa;
+//number - 2 power 53
+//which are not defined -> undefined 
+//but NULL is a value or 0 , its type is OBJECT !!!!!
+
+console.log(typeof age);
+console.table([typeof name, typeof age, typeof isMarried, typeof hasJob, typeof isa])
+
---
+++ 01_basics/variables.js
@@ -0,0 +1,17 @@
+const accountId = 12345//cant be changed
+let accountEmail = "jk@1234"
+//these 2 are good methods of variable assignment
+
+var accountPw = "345JK" //var is not used nowadays bacz of block scope and functional scope
+accountCity = "cuttack" // not recommended but can be done
+let accountName
+
+//accoutId = 33555 shows error for changing
+console.log(accountId);
+
+accountEmail = "jks@gmail.com"
+accountPw = "123jpjp"
+accountCity = "BBSR"
+console.log(accountEmail);// shows error why??? 
+console.table([accountId, accountEmail, accountPw, accountCity, accountName])
+
